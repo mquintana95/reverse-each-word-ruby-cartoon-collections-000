b9e97af603ab185da8c3d6ec3d4a18c14202b18e
Marcus Quintana <mq97325@gmail.com>
2020-09-18 18:40:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-def reverse_each_word(backwards)
-  backwards.each do |string|
-    str.split("")
+def reverse_each_word(sentence)
+  sentence.each do |word|
+    word.split("")
     word.reverse
   end
 end
