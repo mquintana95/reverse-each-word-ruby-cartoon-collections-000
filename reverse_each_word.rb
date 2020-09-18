8506927f217729aa3254f21227401a77dac8fb7b
Marcus Quintana <mq97325@gmail.com>
2020-09-18 18:50:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   array = string.split(" ")
   new_array = []
   original_array.each do|string|
-    return_array << string.reverse
+    new_array << string.reverse
   end
   return_array
 end
