39c1919eb4527a8b5e579b7bc3c710b12fc7ab9c
Marcus Quintana <mq97325@gmail.com>
2020-09-18 18:52:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
-  original_array.each do|string|
+  array.each do|string|
     new_array << string.reverse
   end
   new_array
